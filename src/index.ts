d1fac1bb92fb803c9d5cddb1152adb616f8bec67
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-07-04 19:12:35 +0545

    05_catching error

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello TS !!');
+const students = [
+  {
+    name: 'John',
+    score: 70,
+  },
+  {
+    name: 'Jane',
+    score: 80,
+  },
+];
+for (const std of students) {
+  console.log(`Student Name: ${std.name}, Score: ${std.score}`);
+}
