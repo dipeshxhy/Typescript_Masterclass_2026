e63edc3a22aa4dc87e6146c16d4c3b62b26d88c8
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-07-04 21:08:16 +0545

    string,number,boolean types done

---
+++ src/010_primitive_type.ts
@@ -0,0 +1,29 @@
+let firstName = 'John';
+// firstName = 12;
+console.log(firstName);
+
+let automobile = 'BMW';
+
+const city = 'Kathmandu';
+// city = 'Lahan'
+
+let students = 30;
+let studentAsString = students.toString();
+
+// number types
+let age = 24;
+let price = 12.5;
+let year = 2026;
+const numberOfMembers = 20;
+
+let stringToNumber = parseInt('123');
+
+// booleans
+// true or false
+
+let isStudent = true;
+// isStudent = 'yes';
+// isStudent = 1;
+
+const alwaysStudent = true;
+let minAge = age > 6 ? true : false;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-function addNumber(a: number, b: number) {
-  return a + b;
-}
-console.log(addNumber(2, 3));
-// console.log(addNumber('text', 3));
-// console.log('text', 2);
-// console.log('text', 3);
+import './010_primitive_type';
