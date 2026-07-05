efed0457eff94043f1987ecf4e4ff7754003f131
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-07-05 14:36:57 +0545

    optional parameters

--- src/039_function.ts
+++ src/039_function.ts
@@ -1,12 +1,16 @@
-function intro(name: string, age: number):string  {
+function intro(name: string, age: number, country?: string): string {
+  if (country) {
+    return `Hello, my name is ${name} and I am ${age} years old. I am from ${country}.`;
+  }
   return `Hello, my name is ${name} and I am ${age} years old.`;
 }
+intro('Alice', 30, 'USA');
 
 // function expression
 const intro2 = (name: string, age: number): string => {
   return `Hello, my name is ${name} and I am ${age} years old.`;
-}
+};
 
 const intro3: (name: string, age: number) => string = (name, age) => {
   return `Hello, my name is ${name} and I am ${age} years old.`;
-}
+};
