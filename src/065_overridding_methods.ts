7f8e8c17a1f71947f16e19b125ec67b29d12c3c6
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-07-06 21:03:53 +0545

    overriding methods in class

---
+++ src/065_overridding_methods.ts
@@ -0,0 +1,48 @@
+class User {
+  public name: string;
+  readonly email: string;
+  lastName?: string | undefined;
+  // Changing the phone to private member
+  private phone: number;
+
+  constructor(name: string, email: string, phone: number, lastName?: string) {
+    this.name = name;
+    this.email = email;
+    this.lastName = lastName;
+    this.phone = phone;
+  }
+
+  greet(): string {
+    return `Hello ${this.name}`;
+  }
+}
+
+class Admin extends User {
+  isAdmin: boolean = true;
+  usersReporting: number;
+
+  constructor(
+    name: string,
+    email: string,
+    phone: number,
+    usersReporting: number,
+    lastName?: string,
+  ) {
+    super(name, email, phone, lastName);
+    this.usersReporting = usersReporting;
+  }
+
+  // A greet methods can override the parent class method
+  // but the condition is that the child class methods must have the same signature
+  override greet(): string {
+    return `Hello ${this.name}! I am the admin.`;
+  }
+}
+
+// Create users with phone numbers now
+const user: User = new User('Mark', 'Mark@email.com', 123456);
+const admin: Admin = new Admin('John', 'John@email.com', 123456, 11);
+
+// Greet methods on both objects would return a different greeting
+console.log(user.greet());
+console.log(admin.greet());
